955a6f0f43545fafac33bf04d0ad2dbbbbd6cd81
ayushmangrv <ayushman@gravityconsulting.com.au>
2018-02-21 17:49:39 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+
+		body{
+
+			font-family: 'Montserrat', sans-serif;
+
+		}
+		
+		#header{
+
+			background-color: black;
+			font-size: 20px;
+			color: white;
+			padding: 50px;
+		}
+		#footer{
+
+			background-color: cyan;
+			font-size: 20px;
+			color: black;
+			padding: 50px;
+		}
+
+
+	</style>
+</head>
+<body>
+
+	<section id="header">
+		<div>
+			
+			HEADER
+
+		</div>
+	</section>
+	<section id="ps">	
+		<div id="p1">
+			
+			<h1>Title 1</h1>
+
+			<p>
+				
+				Content for p1
+
+			</p>
+
+		</div>
+		<div id="p2">
+			
+			<h1>Title 2</h1>
+
+			<p>
+				
+				Content for p2
+
+			</p>
+		</div>		
+	</section>	
+	<section id="footer">	
+		<div>
+			
+			<img src="http://readcomiconline.to/Content/images/logo.png" height="50px" width="auto" />
+			FOOTER
+
+		</div>
+	</section>
+
+</body>
+</html>
+?>
